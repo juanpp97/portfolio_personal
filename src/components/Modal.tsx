66e8a0cb0909f14153a907b331e82872aa02b2b8
Juan P. Peñaloza <juanpenaloza97@gmail.com>
2024-07-18 16:48:11 -0300

    Fix build

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,100 +1,100 @@
-import '@/components/styles/modal.css'
-import { useEffect } from 'preact/hooks';
-import Glide from "@glidejs/glide";
+// import { useEffect } from 'preact/hooks';
+// import Glide from "@glidejs/glide";
+// import '@/components/styles/modal.css'
 
 
-interface Props {
-  images: Array<string>;
-  onClose: () => void;
-}
+// interface Props {
+//   images: Array<string>;
+//   onClose: () => void;
+// }
 
-export default function Modal({ images, onClose }: Props) {
-  const handleEscape = (event: KeyboardEvent) => {
-    if(event.key !== "Escape") return
-    onClose()
-  }
-  useEffect(() => {
-    let glide:any;
-    let content = document.getElementById("content");
-    setTimeout(() => {
-      glide = new Glide(".glide");
-      glide.mount();
-      if(content) content.style.opacity = "1";
-    }, 10);
-    document.addEventListener("keydown", handleEscape)
-    return () => {
-      glide.destroy()
-      if(content) content.style.opacity = "0";
-      document.removeEventListener("keydown", handleEscape)
-    };
-  })
+// export default function Modal({ images, onClose }: Props) {
+//   const handleEscape = (event: KeyboardEvent) => {
+//     if(event.key !== "Escape") return
+//     onClose()
+//   }
+//   useEffect(() => {
+//     let glide:any;
+//     let content = document.getElementById("content");
+//     setTimeout(() => {
+//       glide = new Glide(".glide");
+//       glide.mount();
+//       if(content) content.style.opacity = "1";
+//     }, 10);
+//     document.addEventListener("keydown", handleEscape)
+//     return () => {
+//       glide.destroy()
+//       if(content) content.style.opacity = "0";
+//       document.removeEventListener("keydown", handleEscape)
+//     };
+//   })
 
-  return (
-    <div className={"overlay"}>
-      <div className="modal__content" id="content">
-      <div class="glide">
-          <span id="modal-close" onClick={onClose}>&times;</span>
+//   return (
+//     <div className={"overlay"}>
+//       <div className="modal__content" id="content">
+//       <div class="glide">
+//           <span id="modal-close" onClick={onClose}>&times;</span>
 
-          <div class="glide__track" data-glide-el="track">
-            <ul class="glide__slides">
-              {images.map((image, index) => (
-                  <li class="glide__slide">
-                    <img src={image} alt={`Slide ${index + 1}`} />
-                  </li>
-              ))}
-              {/* <li class="glide__slide">
-                <img src="./DSH/DSH1.webp" />
-              </li>
-              <li class="glide__slide">
-                <img src="./DSH/DSH2.webp" />
-              </li>
-              <li class="glide__slide">
-                <img src="./DSH/DSH3.webp" />
-              </li>
-              <li class="glide__slide">
-                <img src="./DSH/DSH4.webp" />
-              </li>
-              <li class="glide__slide">
-                <img src="./DSH/DSH5.webp" />
-              </li>
-              <li class="glide__slide">
-                <img src="./DSH/DSH6.webp" />
-              </li> */}
-            </ul>
-          </div>
-          <div class="glide__arrows" data-glide-el="controls">
-            <button class="glide__arrow glide__arrow--left" data-glide-dir="<">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
+//           <div class="glide__track" data-glide-el="track">
+//             <ul class="glide__slides">
+//               {images.map((image, index) => (
+//                   <li class="glide__slide">
+//                     <img src={image} alt={`Slide ${index + 1}`} />
+//                   </li>
+//               ))}
+//               {/* <li class="glide__slide">
+//                 <img src="./DSH/DSH1.webp" />
+//               </li>
+//               <li class="glide__slide">
+//                 <img src="./DSH/DSH2.webp" />
+//               </li>
+//               <li class="glide__slide">
+//                 <img src="./DSH/DSH3.webp" />
+//               </li>
+//               <li class="glide__slide">
+//                 <img src="./DSH/DSH4.webp" />
+//               </li>
+//               <li class="glide__slide">
+//                 <img src="./DSH/DSH5.webp" />
+//               </li>
+//               <li class="glide__slide">
+//                 <img src="./DSH/DSH6.webp" />
+//               </li> */}
+//             </ul>
+//           </div>
+//           <div class="glide__arrows" data-glide-el="controls">
+//             <button class="glide__arrow glide__arrow--left" data-glide-dir="<">
+//               <svg
+//                 xmlns="http://www.w3.org/2000/svg"
                 
    
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                stroke-width="2"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                className="icon icon-tabler icons-tabler-outline icon-tabler-chevron-left arrow"
-              ><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><path
-                d="M15 6l-6 6l6 6"></path></svg>
-            </button>
-            <button class="glide__arrow glide__arrow--right" data-glide-dir=">">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
+//                 viewBox="0 0 24 24"
+//                 fill="none"
+//                 stroke="currentColor"
+//                 stroke-width="2"
+//                 stroke-linecap="round"
+//                 stroke-linejoin="round"
+//                 className="icon icon-tabler icons-tabler-outline icon-tabler-chevron-left arrow"
+//               ><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><path
+//                 d="M15 6l-6 6l6 6"></path></svg>
+//             </button>
+//             <button class="glide__arrow glide__arrow--right" data-glide-dir=">">
+//               <svg
+//                 xmlns="http://www.w3.org/2000/svg"
 
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                stroke-width="2"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                className="icon icon-tabler icons-tabler-outline icon-tabler-chevron-right arrow"
-              ><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><path
-                d="M9 6l6 6l-6 6"></path></svg>
-            </button>
-          </div>
-        </div>
-      </div>
-      </div>
-  )
-}
+//                 viewBox="0 0 24 24"
+//                 fill="none"
+//                 stroke="currentColor"
+//                 stroke-width="2"
+//                 stroke-linecap="round"
+//                 stroke-linejoin="round"
+//                 className="icon icon-tabler icons-tabler-outline icon-tabler-chevron-right arrow"
+//               ><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><path
+//                 d="M9 6l6 6l-6 6"></path></svg>
+//             </button>
+//           </div>
+//         </div>
+//       </div>
+//       </div>
+//   )
+// }
--- src/components/Proyectos.tsx
+++ src/components/Proyectos.tsx
@@ -1,14 +1,11 @@
 import { useState } from 'preact/hooks';
 import { getI18N } from '@/i18n';
-import DOMPurify from 'isomorphic-dompurify';
-import Button from '@/components/Button'
 import ProjectCard from '@/components/ProjectCard';
 import Modal from '@/components/Modal'
 import '@/components/styles/proyectos.css'
 
 interface Props {
     currentLocale?: string;
-    // messages?: Array<{name: string, description: string, technologies: Array<string>, images:Array<string>, github: string, deploy: string}>;
     gallery?: preact.ComponentChildren;
     github?: preact.ComponentChildren;
     deploy?: preact.ComponentChildren;
@@ -19,20 +16,21 @@ interface Props {
     const i18n = getI18N({ currentLocale })["projects"];
     const total = i18n.detail.length;
 
-    const openModal = (images: Array<string>) => {
-      setImagesModal(images);
-    }
+    // const openModal = (images: Array<string>) => {
+    //   setImagesModal(images);
+    // }
 
-    const closeModal = () => {
-      setImagesModal([]);
-    }
+    // const closeModal = () => {
+    //   setImagesModal([]);
+    // }
 
     return (
 
         <section id="projects">
           <h1 className="heading"> {i18n.title} </h1>
           {i18n.detail.slice(0, count).map((project) => (
-            <ProjectCard currentLocale={currentLocale} project={project} gallery={gallery} github={github} deploy={deploy} onClick={() => openModal(project.images)} />
+             <ProjectCard currentLocale={currentLocale} project={project} gallery={gallery} github={github} deploy={deploy} />
+            // <ProjectCard currentLocale={currentLocale} project={project} gallery={gallery} github={github} deploy={deploy} onClick={() => openModal(project.images)} />
           ))}
 
           {(total - count) > 0 && 
@@ -40,7 +38,8 @@ interface Props {
             <button onClick={() => setCount(count + 3)} className={"more__btn"}>{i18n.load_more}</button>
           </div>
           }
-        {imagesModal.length > 0 && <Modal images={imagesModal} onClose = {closeModal}/>}
+
+        {/* {imagesModal.length > 0 && <Modal images={imagesModal} onClose = {closeModal}/>} */}
         </section>
     )
 }
