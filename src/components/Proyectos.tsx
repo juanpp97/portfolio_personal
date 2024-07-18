c98532eca4a0b770ca0b0fcdcb8a006b8525fc3d
Juan P. Peñaloza <juanpenaloza97@gmail.com>
2024-07-18 16:53:33 -0300

    Revert "Fix build"
    This reverts commit 66e8a0cb0909f14153a907b331e82872aa02b2b8.

--- src/components/Proyectos.tsx
+++ src/components/Proyectos.tsx
@@ -1,11 +1,14 @@
 import { useState } from 'preact/hooks';
 import { getI18N } from '@/i18n';
+import DOMPurify from 'isomorphic-dompurify';
+import Button from '@/components/Button'
 import ProjectCard from '@/components/ProjectCard';
 // import Modal from '@/components/Modal'
 import '@/components/styles/proyectos.css'
 
 interface Props {
     currentLocale?: string;
+    // messages?: Array<{name: string, description: string, technologies: Array<string>, images:Array<string>, github: string, deploy: string}>;
     gallery?: preact.ComponentChildren;
     github?: preact.ComponentChildren;
     deploy?: preact.ComponentChildren;
@@ -29,8 +32,7 @@ interface Props {
         <section id="projects">
           <h1 className="heading"> {i18n.title} </h1>
           {i18n.detail.slice(0, count).map((project) => (
-             <ProjectCard currentLocale={currentLocale} project={project} gallery={gallery} github={github} deploy={deploy} />
-            // <ProjectCard currentLocale={currentLocale} project={project} gallery={gallery} github={github} deploy={deploy} onClick={() => openModal(project.images)} />
+            <ProjectCard currentLocale={currentLocale} project={project} gallery={gallery} github={github} deploy={deploy} onClick={() => openModal(project.images)} />
           ))}
 
           {(total - count) > 0 && 
@@ -38,8 +40,7 @@ interface Props {
             <button onClick={() => setCount(count + 3)} className={"more__btn"}>{i18n.load_more}</button>
           </div>
           }
-
-        {/* {imagesModal.length > 0 && <Modal images={imagesModal} onClose = {closeModal}/>} */}
+        {imagesModal.length > 0 && <Modal images={imagesModal} onClose = {closeModal}/>}
         </section>
     )
 }
